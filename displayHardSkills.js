1ac73f481404e7362d9afb2f32341068b8eec36d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-05 16:27:42 +0100

    Refactorisation : modification nom d'une variable dans displayHardSkills.js

--- displayHardSkills.js
+++ displayHardSkills.js
@@ -4,7 +4,7 @@ async function displayHardSkills() {
         const hardSkills = await response.json();
         JSON.stringify(hardSkills, null, 2);
 
-        const container = document.getElementById('hard-skills-container');
+        const hardSkillsContainer = document.getElementById('hard-skills-container');
         hardSkills.forEach(skill => {
             const span = document.createElement('span');
             const img = document.createElement('img');
@@ -12,7 +12,7 @@ async function displayHardSkills() {
             img.classList.add('hard-skills-img');
             span.appendChild(img);
             span.appendChild(document.createTextNode(skill.name));
-            container.appendChild(span);
+            hardSkillsContainer.appendChild(span);
         });
     } catch (error) {
         console.error('Erreur lors du chargement des hard skills:', error);
