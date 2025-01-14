8e5ec78fd8fea898b93da15e173f6ac9e425497b
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-14 08:35:18 +0100

    Feature : transfert section À propos dans HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ function HomePage() {
             <h1>Page d'accueil</h1>
             <h1>Natasha Finkelsztajn</h1>
             <p className="sous-titre">Développeur ReactJS</p>
+
+            <section>
+                <h2>À propos de moi</h2>
+                <div className="about-me-container">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi nulla, facilisis vel erat eget, porta tempor libero. Curabitur risus purus, aliquam sit leo.</p>
+                </div>
+            </section>
+
         </div>
     )
 }
