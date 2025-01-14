c100598788b62f77a8d799e9f13982386b57cd0d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-14 09:26:33 +0100

    Clean code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 function HomePage() {
     return (
         <div>
-            <h1>Natasha Finkelsztajn</h1>
-            <p className="sous-titre">Développeur ReactJS</p>
+            <main>
+                <h1>Natasha Finkelsztajn</h1>
+                <p className="sous-titre">Développeur ReactJS</p>
 
-            <section>
-                <h2>À propos de moi</h2>
-                <div className="about-me-container">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi nulla, facilisis vel erat eget, porta tempor libero. Curabitur risus purus, aliquam sit leo.</p>
-                </div>
-            </section>
-
+                <section>
+                    <h2>À propos de moi</h2>
+                    <div className="about-me-container">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi nulla, facilisis vel erat eget, porta tempor libero. Curabitur risus purus, aliquam sit leo.</p>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
