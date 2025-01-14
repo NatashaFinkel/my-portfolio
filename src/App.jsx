c06d9e02a7df26d49b993970e7586de9ff335ac2
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-14 09:49:19 +0100

    Feature : implémentation du composant Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppRouter from './components/AppRouter';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <AppRouter />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+function Footer() {
+    return (
+        <footer className="contact-page-main-container">
+            <section className="social-media-container">
+
+                <div className="linkedin-link media">
+                    <a href="https://www.linkedin.com/in/natasha-finkelsztajn" target="_blank">Mon profil Linkedin</a>
+                </div>
+                <div className="GitHub-link media">
+                    <a href="https://github.com/NatashaFinkel" target="_blank">Mon profil GitHub</a>
+                </div>
+            </section>
+
+            <section>
+                <div className="contact-container" id="contact-container">
+                    <p>Natasha Finkelsztajn</p>
+                    <p>32 rue des Rosiers, <br></br>75004 Paris.</p>
+                    <b>06 24 25 07 18</b>
+                    <span>
+                        <a href="mailto:nfinkelsztajn@gmail.com" target="_blank">nfinkelsztajn@gmail.com</a>
+                    </span>
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
