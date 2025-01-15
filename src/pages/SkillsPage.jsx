e1c31b79068458a8f118baacfb0b95941c3fda2b
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-15 16:35:27 +0100

    Clean code et correction syntaxe

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -2,10 +2,10 @@ function SkillsPage() {
     return (
         <div>
             <h2>HardSkills</h2>
-            <div class="hard-skills-container" id="hard-skills-container"></div>
+            <div className="hard-skills-container" id="hard-skills-container"></div>
 
             <h2>SoftSkills</h2>
-            <div class="soft-skills-container" id="soft-skills-container"></div>
+            <div className="soft-skills-container" id="soft-skills-container"></div>
         </div>
     )
 }
