233b76cabd842d7d5708e2256beaa94521f9cac9
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-18 13:26:33 +0100

    Refacto : réécriture de displayHardSkills()

--- displayHardSkills.js
+++ displayHardSkills.js
@@ -1,44 +1,34 @@
-async function displayHardSkills() {
-    let pageURL = window.location;
-    let currentPage = pageURL.pathname;
-    try {
-        if (currentPage === "/my-portfolio/skills") {
-            const response = await fetch('hardSkills.json');
-            const hardSkills = await response.json();
-            JSON.stringify(hardSkills, null, 2);
+import hardSkills from './hardSkills.json';
 
-            const hardSkillsContainer = document.getElementById('hard-skills-container');
+export default function displayHardSkills() {
+    const hardSkillsContainer = document.getElementById('hard-skills-container');
+    hardSkills.forEach(skill => {
+        const span = document.createElement('span');
+        span.classList.add('skill');
+        const img = document.createElement('img');
+        img.src = skill.imgSrc;
+        img.classList.add('hard-skills-img');
 
-            hardSkills.forEach(skill => {
-                const span = document.createElement('span');
-                span.classList.add('skill');
-                const img = document.createElement('img');
-                img.src = skill.imgSrc;
-                img.classList.add('hard-skills-img');
-                span.appendChild(img);
-                const txtDiv = document.createElement('div');
-                txtDiv.classList.add('skill-txt');
-                txtDiv.appendChild(document.createTextNode(skill.name));
-                span.appendChild(txtDiv);
-                const tooltip = document.createElement('div');
-                tooltip.className = 'tooltip';
-                tooltip.textContent = skill.name;
-                span.appendChild(tooltip);
-                span.classList.add('tooltip-container');
-                hardSkillsContainer.appendChild(span);
-                const skills = document.querySelectorAll(".skill");
+        span.appendChild(img);
+        const txtDiv = document.createElement('div');
+        txtDiv.classList.add('skill-txt');
+        txtDiv.appendChild(document.createTextNode(skill.name));
+        span.appendChild(txtDiv);
+        const tooltip = document.createElement('div');
+        tooltip.className = 'tooltip';
+        tooltip.textContent = skill.name;
+        span.appendChild(tooltip);
+        span.classList.add('tooltip-container');
 
-                skills.forEach((item, index) => {
-                    setTimeout(() => {
-                        item.classList.add("visible");
-                    }, index * 400);
-                });
+        hardSkillsContainer.appendChild(span);
+    });
 
-            });
-        }
-    } catch (error) {
-        console.error('Erreur lors du chargement des hard skills: ', error);
-    }
+    const skills = document.querySelectorAll(".skill");
+    const timeouts = [];
+    skills.forEach((item, index) => {
+        const timeout = setTimeout(() => {
+            item.classList.add("visible");
+        }, index * 400);
+        timeouts.push(timeout);
+    });
 }
-
-displayHardSkills();
