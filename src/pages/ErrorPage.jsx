411c0e9f5dc0774a7e2ed75989b3afa63813a567
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-18 19:14:19 +0100

    Feature : implémentation de la page d'erreur

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,22 @@
+import errorImage from '/assets/404-img.png?url.';
+
 function ErrorPage() {
     return (
         <main>
-            <h1>Page d'erreur</h1>
-        </main>
+            <h1>OOPS !</h1>
+            <span className='error-txt-div'>
+                <p >La page que vous cherchez n'existe pas !</p>
+            </span>
+
+            <div className='error-txt-div'>
+                <p>Vous pouvez retourner à la page d'accueil en cliquant <button onClick={() => window.location.href = "/"} className="get-back-btn">ici</button></p>
+            </div>
+
+            <div>
+                <img src={errorImage} alt="illustration ERREUR 404" className="error-img"></img>
+            </div>
+        </main >
     )
-}
+};
 
 export default ErrorPage;
