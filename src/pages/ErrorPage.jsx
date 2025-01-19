b60d0fd606a19e7d201deef90ea2c505cf465370
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-19 11:01:40 +0100

    Fix : Redirection dans le composant ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,21 +1,33 @@
 import errorImage from '/assets/404-img.png?url.';
 
 function ErrorPage() {
+    function getRedirectUrl() {
+        let redirectUrl;
+        if (window.location.origin === "https://natashafinkel.github.io/my-portfolio\d+/.test") {
+            redirectUrl = "https://natashafinkel.github.io/my-portfolio/";
+        } else {
+            redirectUrl = window.location.origin + "/my-portfolio/";
+            return redirectUrl;
+        }
+    }
+
     return (
         <main>
             <h1>OOPS !</h1>
             <span className='error-txt-div'>
-                <p >La page que vous cherchez n'existe pas !</p>
+                <p>La page que vous cherchez n'existe pas !</p>
             </span>
 
             <div className='error-txt-div'>
-                <p>Vous pouvez retourner à la page d'accueil en cliquant <button onClick={() => window.location.href = "/"} className="get-back-btn">ici</button></p>
+                <p>Vous pouvez retourner à la page d'accueil en cliquant
+                    <button onClick={() => window.location.href = getRedirectUrl()} className='get-back-btn'>ici</button>
+                </p>
             </div>
 
             <div>
                 <img src={errorImage} alt="illustration ERREUR 404" className="error-img"></img>
             </div>
-        </main >
+        </main>
     )
 };
 
