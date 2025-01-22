a2ea6161e92a52fa5428931a5da746f3e3f0c921
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-22 14:39:44 +0100

    Feature : implémentation de la redirection de la page formation à la page expérience professionnelle

--- src/pages/FormationPage.jsx
+++ src/pages/FormationPage.jsx
@@ -1,7 +1,23 @@
 import EducationComponent from "../components/EducationComponent";
+
 function FormationPage() {
+    function getToPastjobsPage() {
+        let redirectUrl;
+        if (window.location.origin === "https://natashafinkel.github.io/my-portfolio/formation") {
+            redirectUrl = "https://natashafinkel.github.io/my-portfolio/past-jobs";
+        } else {
+            redirectUrl = window.location.origin + "/my-portfolio/past-jobs";
+            return redirectUrl;
+        }
+    }
+
     return (
         <main>
+            <h1>Diplômes</h1>
+            <div className="formation-txt-div">
+                <p>Pour savoir ce que j'ai fait entre l'obtention de mon Baccalauréat et mon diplôme de Développeur intégrateur web, cliquez<button onClick={() => window.location.href = getToPastjobsPage()} className='get-back-btn'>ici</button>
+                </p>
+            </div>
             <EducationComponent />
         </main>
     )
