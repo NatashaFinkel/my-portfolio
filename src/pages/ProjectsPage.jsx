f801ed66bbe4e2bd9872a6f6db40561ee022621a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-02-01 17:58:07 +0100

    Feature : ajout des img de la page Projets

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,3 +1,8 @@
+import wildFlowersProjectImg from '/assets/fleurs-sauvages.webp';
+import cutePetProjectImg from '/assets/cute-pet-project.png';
+import ninaCarducciProject from '/assets/camera.webp';
+import logoOpenClassRooms from '/assets/logo-openClassRooms.webp';
+
 function ProjectsPage() {
     return (
         <main>
