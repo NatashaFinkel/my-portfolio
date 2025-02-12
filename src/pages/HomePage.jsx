ff314f44a07f9979b73f7ecfaf7bc30e2eca6ed3
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-02-12 16:43:29 +0100

    Refacto : amélioration des noms des variables css et clean code css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,22 +2,21 @@ import AppNavLink from "../components/NavLink";
 
 function HomePage() {
     return (
-        <main className="l-2">
-            <h1 className="l-1">Natasha Finkelsztajn.<br />
-                <span className="typed">Développeur web,</span>
+        <main className="landing-page">
+            <h1 className="landing-page-title">Natasha Finkelsztajn.<br />
+                <span>Développeur web,</span>
                 <br />
-                <span className="l-6">spécialiste React.js.</span>
+                <span>spécialiste React.js.</span>
             </h1>
 
-            <div className="l-5">
-                <span className="l-3">
-                    <AppNavLink
-                        navLinkDirection="/projects"
-                        navLinkActiveclassname="active"
-                        navLinkTxt="Mes projets"
-                    />
-                </span>
-            </div>
+            <span className="landing-page-project-page-link-div">
+                <AppNavLink
+                    navLinkDirection="/projects"
+                    navLinkClassName="landing-page-project-page-link"
+                    navLinkActiveclassname="active"
+                    navLinkTxt="Mes projets"
+                />
+            </span>
         </main>
     );
 }
